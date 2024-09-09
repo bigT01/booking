820abd2cbe531c99c27d1d57f1115dd343be0c01
tan <tanat8511@gmail.com>
2024-09-09 16:50:56 +0500

    BREAKING CHANGE: basic styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,30 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const nunitoSans = localFont({
+  src: [
+    {
+      path: './fonts/NunitoSans-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/NunitoSans-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './fonts/NunitoSans-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './fonts/NunitoSans-ExtraBold.ttf',
+      weight: '800',
+      style: 'normal',
+    },
+  ],
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +40,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${nunitoSans.className} antialiased`}
       >
         {children}
       </body>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,71 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'white': '#FFFFFF',
+        'blue': '#407AEA',
+        'purple-dark': '#1513A0',
+        'purple-blue': '#605DEC',
+        'purple-light': '#BCBAF7',
+        'purple-extra-light':'#E9E8FC',
+        'purple-white':'#F6F6FE',
+        'purple-grey': '#52527A20',
+        'purple-white-light': '#FAFAFE',
+        'deep-green': '#007B65',
+        'turquoise': '#5CD6C0',
+        'red': '#EB5757',
+        'red-light': '#FDEFEF',
+        'pink-gradient': 'linear-gradient(180deg, rgba(235,86,140,1) 0%, rgba(237,94,118,1) 100%)',
+        'purple-blue-gradient': 'linear-gradient(180deg, rgba(96,93,236,1) 0%, rgba(42,38,217,1) 100%)',
+        'turquoise-gradient': 'linear-gradient(180deg, rgba(92,214,192,1) 0%, rgba(34,195,166,1) 100%)',
+        grey : {
+          900: '#27273F',
+          800: '#36374A',
+          700: '#52556F',
+          600: '#6E7491',
+          500: '#657491',
+          400: '#7C8DB0',
+          300: '#A1B0CC',
+          200: '#CBD4E6',
+          100: '#FAFAFA'
+        }
+      },
+      fontSize: {
+        'xs': ['12px', {
+          fontWeight: '400',
+        }],
+        'sm': ['14px', {
+          fontWeight: '400',
+        }],
+        'base': ['16px', {
+          fontWeight: '400',
+        }],
+        'lg': ['18px', {
+          fontWeight: '400',
+        }],
+        'xl': ['24px', {
+          fontWeight: '800',
+        }],
+        'header': ['18px', {
+          fontWeight: '700',
+        }],
+        'h1': ['36px', {
+          fontWeight: '600',
+        }],
+        'h2': ['32px', {
+          fontWeight: '600',
+        }],
+        'h3': ['24px', {
+          fontWeight: '600',
+        }],
+        'h4': ['18px', {
+          fontWeight: '600',
+        }],
+        'h5': ['16px', {
+          fontWeight: '600',
+        }],
+        'h6': ['14px', {
+          fontWeight: '600',
+        }],
       },
     },
   },
