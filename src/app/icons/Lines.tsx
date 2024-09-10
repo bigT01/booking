b31eeb5e85d61e4abae13068181275ca5bb58179
tan <tanat8511@gmail.com>
2024-09-10 17:43:28 +0500

    feat(UI): to button add icon

---
+++ src/app/icons/Lines.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+type IconsProps = {
+    size: number
+}
+
+export const ArrowRight = ({size=18}:IconsProps) => {
+    return (
+        <svg width={size} height={(size / 22) * 18} viewBox="0 0 22 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 9H20.5M20.5 9L13 1.5M20.5 9L13 16.5" stroke="#6E7491" strokeWidth="2" strokeLinecap="round"
+                  strokeLinejoin="round"/>
+        </svg>
+    );
+};
+
+export const ArrowLeft = ({size=18}:IconsProps) => {
+    return (
+        <svg width={size} height={(size / 22) * 18} viewBox="0 0 22 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M20.5 9H1M1 9L8.5 1.5M1 9L8.5 16.5" stroke="#6E7491" strokeWidth="2" strokeLinecap="round"
+                  strokeLinejoin="round"/>
+        </svg>
+
+    );
+};
+
+
--- src/app/shared/components/UI/Button.tsx
+++ src/app/shared/components/UI/Button.tsx
@@ -25,25 +25,28 @@ const Button = ({type, size, icon, state='default', children}:ButtonProps) => {
         }
     } = {
         Primary: {
-            default: "bg-purple-blue text-grey-100 hover:bg-purple-dark hover:text-grey-100",
-            inactive: "bg-[#CBD4E630] border-[1px] border-grey-400 text-grey-400 hover:bg-purple-dark hover:text-grey-100",
-            destructive: "bg-red text-grey-100 hover:bg-purple-dark hover:text-grey-100"
+            default: "bg-purple-blue btn-primary-default text-grey-100 hover:bg-purple-dark hover:text-grey-100",
+            inactive: "bg-[#CBD4E630] btn-primary-inactive border-[1px] border-grey-400 text-grey-400 hover:bg-purple-dark hover:text-grey-100",
+            destructive: "bg-red text-grey-100 btn-primary-default hover:bg-purple-dark hover:text-grey-100"
         },
         Secondary: {
-            default: "bg-transparent border-[1px] border-purple-blue text-purple-blue hover:bg-purple-extra-light hover:text-purple-blue",
-            inactive: "bg-transparent border-[1px] border-grey-400 text-grey-400 hover:bg-purple-extra-light hover:text-purple-blue",
-            destructive: "bg-transparent border-[1px] border-red text-red hover:bg-purple-extra-light hover:text-purple-blue"
+            default: "bg-transparent border-[1px] btn-secondary-default border-purple-blue text-purple-blue hover:bg-purple-extra-light hover:text-purple-blue",
+            inactive: "bg-transparent border-[1px] btn-primary-inactive border-grey-400 text-grey-400 hover:bg-purple-extra-light hover:text-purple-blue",
+            destructive: "bg-transparent border-[1px] btn-secondary-destructive border-red text-red hover:bg-purple-extra-light hover:text-purple-blue"
         },
         Tertiary: {
-            default: "bg-transparent text-purple-blue hover:bg-purple-white hover:text-purple-dark",
-            inactive: "bg-transparent text-grey-400 hover:bg-purple-white hover:text-purple-dark",
-            destructive: "bg-transparent text-red hover:bg-purple-white hover:text-purple-dark"
+            default: "bg-transparent btn-tertiary-default text-purple-blue hover:bg-purple-white hover:text-purple-dark",
+            inactive: "bg-transparent text-grey-400 btn-primary-inactive hover:bg-purple-white hover:text-purple-dark",
+            destructive: "bg-transparent text-red btn-secondary-destructive hover:bg-purple-white hover:text-purple-dark"
         }
     };
 
     return (
-        <button className={`rounded-md ${sizeStyles[size]} ${typeStyles[type][state]} transition-colors`}>
-            {children}
+        <button className={`rounded-md flex items-center gap-1 ${sizeStyles[size]} ${typeStyles[type][state]} transition-colors`}>
+            <span>
+                {children}
+            </span>
+            {icon ? icon : null}
         </button>
     );
 };
