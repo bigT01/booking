d3a6fb28f41f19ad0eaa2e7310d0984a729a8cc8
tan <tanat8511@gmail.com>
2024-10-04 01:52:33 +0500

    feat(calendar): second calendar

--- src/app/shared/components/UI/RoundTripCalendar.tsx
+++ src/app/shared/components/UI/RoundTripCalendar.tsx
@@ -9,21 +9,28 @@ import CalendarLayout from "@/app/shared/components/UI/CalendarLayout";
 
 const RoundTripCalendar = () => {
     const [firstYearAndMonth, setFirstYearAndMonth] = useState<yearAndMonthFormat | null>(null)
-    const [indexFromCurrentMonth, setIndexFromCurrentMonth] = useState<number>(0)
-    const [firstSelectedDate, setFirstSelectedDate] = useState<selectedDateFormat | null>(null)
+    const [secondYearAndMonth, setSecondYearAndMonth] = useState<yearAndMonthFormat | null>(null)
 
-    const [calendarArray, setCalendarArray] = useState([])
+    const [indexFromCurrentMonth, setIndexFromCurrentMonth] = useState<number>(2)
+
+    const [firstSelectedDate, setFirstSelectedDate] = useState<selectedDateFormat | null>(null)
+    const [secondSelectedDate, setSecondSelectedDate] = useState<selectedDateFormat | null>(null)
+
+    const [firstCalendarDays, setFirstCalendarDays] = useState([])
+    const [secondCalendarDays, setSecondCalendarDays] = useState([])
 
 
     useEffect(() => {
         setFirstYearAndMonth(convertedDateToMonthAndYears(indexFromCurrentMonth))
+        setSecondYearAndMonth(convertedDateToMonthAndYears(indexFromCurrentMonth + 1))
     }, [indexFromCurrentMonth]);
 
     useEffect(() => {
-        if (firstYearAndMonth) {
-            setCalendarArray(getCalendarArray(firstYearAndMonth.year, firstYearAndMonth.month))
+        if (firstYearAndMonth && secondYearAndMonth) {
+            setFirstCalendarDays(getCalendarArray(firstYearAndMonth.year, firstYearAndMonth.month))
+            setSecondCalendarDays(getCalendarArray(secondYearAndMonth.year, secondYearAndMonth.month))
         }
-    }, [firstYearAndMonth]);
+    }, [firstYearAndMonth, secondYearAndMonth]);
 
     const handleFirstDaySelect = (day: number) => {
         if (firstYearAndMonth?.year && firstYearAndMonth.month) {
@@ -32,27 +39,57 @@ const RoundTripCalendar = () => {
         }
     }
 
+    const handleSecondDaySelect = (day: number) => {
+        if (secondYearAndMonth?.year && secondYearAndMonth.month) {
+            const newDate = {day: day, year: secondYearAndMonth.year, month: secondYearAndMonth.month}
+            setSecondSelectedDate(toggleSelectedDate(secondSelectedDate, newDate))
+        }
+    }
+
     return (
-        <div className={'p-4 w-fit'}>
-            <h5 className='mb-2 text-left'>{firstYearAndMonth?.month} {firstYearAndMonth?.year}</h5>
-            <CalendarLayout>
-                {
-                    calendarArray.map((week, index) => (
-                        <tr key={index}>
-                            {week.map((day: CalendarDayFormat) => (
-                                <td key={day.day}
-                                    className={`w-[32px] h-[32px] rounded-full
+        <div className='flex justify-between'>
+            <div className={'p-4 w-fit'}>
+                <h5 className='mb-2 text-left'>{firstYearAndMonth?.month} {firstYearAndMonth?.year}</h5>
+                <CalendarLayout>
+                        {
+                            firstCalendarDays.map((week, index) => (
+                                <tr key={index}>
+                                    {week.map((day: CalendarDayFormat) => (
+                                        <td key={day.day}
+                                            className={`w-[32px] h-[32px] rounded-full
                                         ${!day.isThisMonth ? 'text-gray-400' : 'cursor-pointer'} 
                                         ${checkingTheActiveDay(day, firstSelectedDate) ? 'bg-purple-blue text-white transition-colors'
-                                        : 'bg-transparent transition-colors'}`}
-                                    onClick={() => day.isThisMonth ? handleFirstDaySelect(day.day) : null}>
-                                    {day.day}
-                                </td>
-                            ))}
-                        </tr>
-                    ))
-                }
-            </CalendarLayout>
+                                                : 'bg-transparent transition-colors'}`}
+                                            onClick={() => day.isThisMonth ? handleFirstDaySelect(day.day) : null}>
+                                            {day.day}
+                                        </td>
+                                    ))}
+                                </tr>
+                            ))
+                        }
+                    </CalendarLayout>
+            </div>
+            <div className={'p-4 w-fit'}>
+                <h5 className='mb-2 text-left'>{secondYearAndMonth?.month} {secondYearAndMonth?.year}</h5>
+                <CalendarLayout>
+                        {
+                            secondCalendarDays.map((week, index) => (
+                                <tr key={index}>
+                                    {week.map((day: CalendarDayFormat) => (
+                                        <td key={day.day}
+                                            className={`w-[32px] h-[32px] rounded-full
+                                        ${!day.isThisMonth ? 'text-gray-400' : 'cursor-pointer'} 
+                                        ${checkingTheActiveDay(day, secondSelectedDate) ? 'bg-purple-blue text-white transition-colors'
+                                                : 'bg-transparent transition-colors'}`}
+                                            onClick={() => day.isThisMonth ? handleSecondDaySelect(day.day) : null}>
+                                            {day.day}
+                                        </td>
+                                    ))}
+                                </tr>
+                            ))
+                        }
+                    </CalendarLayout>
+            </div>
         </div>
     );
 };
--- src/utils/ConvertedDate.ts
+++ src/utils/ConvertedDate.ts
@@ -15,14 +15,6 @@ const listOfMonth = [
     'December'
 ];
 
-export function getCurrentMonthAndYear(): {year: number, month: string }{
-    const dateNow = new Date();
-    const year = dateNow.getFullYear();
-    const month = listOfMonth[dateNow.getMonth()];
-
-    return {year, month};
-}
-
 export function convertedDateToMonthAndYears(indexFromCurrentMonth: number): {year: number, month: string }{
     const {year, month} = getCurrentMonthAndYear()
     const fullMonth = listOfMonth.indexOf(month) + indexFromCurrentMonth
@@ -40,36 +32,24 @@ export function convertedDateToMonthAndYears(indexFromCurrentMonth: number): {ye
     return {year: year+yearsShouldBeAdded, month:listOfMonth[monthValue]};
 }
 
-function getFirstDayOfMonth(year:number, month:string): number {
-    const firstDateOfMonth = new Date(year, listOfMonth.indexOf(month), 1);
-    return firstDateOfMonth.getDay();
-}
-
-function getDaysInMonth(year:number, month:string) {
-    return new Date(year, listOfMonth.indexOf(month), 0).getDate();
-}
-
 export function getCalendarArray(year:number, month:string):{day: number, isThisMonth: boolean}[][] {
-    const daysInCurrentMonth = getDaysInMonth(year, month); // Количество дней в текущем месяце
-    const daysInPreviousMonth = getDaysInMonth(year, listOfMonth[listOfMonth.indexOf(month) - 1]); // Количество дней в предыдущем месяце
-    const firstDay = getFirstDayOfMonth(year, month); // На какой день недели начинается месяц
-    const weeks: {day: number, isThisMonth: boolean}[][] = []; // Массив для недель
+    const daysInCurrentMonth = getDaysInMonth(year, month);
+    const daysInPreviousMonth = getDaysInMonth(year, listOfMonth[listOfMonth.indexOf(month) - 1]);
+    const firstDay = getFirstDayOfMonth(year, month);
+    const weeks: {day: number, isThisMonth: boolean}[][] = [];
 
-    let week: CalendarDayFormat[] = []; // Текущая неделя
-    let dayCounter = 1; // Счётчик дней текущего месяца
+    let week: CalendarDayFormat[] = [];
+    let dayCounter = 1;
 
-    // Добавляем дни предыдущего месяца в первую неделю
     for (let i = firstDay; i > 0; i--) {
-        week.push({day:daysInPreviousMonth -i + 1, isThisMonth:false}); // Заполняем недостающие дни в первой неделе
+        week.push({day:daysInPreviousMonth -i + 1, isThisMonth:false});
     }
 
-    // Заполняем оставшиеся дни текущего месяца в первую неделю
     for (let i = firstDay; i < 7; i++) {
         week.push({day:dayCounter++, isThisMonth:true});
     }
-    weeks.push(week); // Добавляем первую неделю
+    weeks.push(week);
 
-    // Заполняем оставшиеся недели
     while (dayCounter <= daysInCurrentMonth) {
         week = [];
         for (let i = 0; i < 7 && dayCounter <= daysInCurrentMonth; i++) {
@@ -78,7 +58,6 @@ export function getCalendarArray(year:number, month:string):{day: number, isThis
         weeks.push(week);
     }
 
-    // Если последняя неделя неполная, добавляем дни следующего месяца
     let nextMonthDay = 1;
     while (weeks[weeks.length - 1].length < 7) {
         weeks[weeks.length - 1].push({day:nextMonthDay++, isThisMonth: false});
@@ -92,4 +71,22 @@ export function getCalendarArray(year:number, month:string):{day: number, isThis
 
 
     return weeks;
+}
+
+
+function getCurrentMonthAndYear(): {year: number, month: string }{
+    const dateNow = new Date();
+    const year = dateNow.getFullYear();
+    const month = listOfMonth[dateNow.getMonth()];
+
+    return {year, month};
+}
+
+function getFirstDayOfMonth(year:number, month:string): number {
+    const firstDateOfMonth = new Date(year, listOfMonth.indexOf(month), 1);
+    return firstDateOfMonth.getDay();
+}
+
+function getDaysInMonth(year:number, month:string) {
+    return new Date(year, listOfMonth.indexOf(month), 0).getDate();
 }
